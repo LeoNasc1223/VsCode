693f5c34c36b51c111fd82cbee02d53feba385f2
LeoNasc1223 <leonardonascimento122312@gmail.com>
2026-01-12 10:28:15 +0000

    estudos

--- estudos_selenium.py
+++ estudos_selenium.py
@@ -1,3 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 navegador = webdriver.Chrome()
+
+navegador.get("https://www.pcdiga.com/")
+
+navegador.maximize_window()
+
+time.sleep(3)
+
+permitir_cookies = navegador.find_element(By.XPATH, "//*[contains(text(), 'Permitir')]")
+permitir_cookies.click()
+
+time.sleep(3)
+
+botao_armazenamento = navegador.find_element(By.XPATH, "//a[contains(., 'Armazenamento')]")
+botao_armazenamento.click()
+
+time.sleep(3)
+
+lista_produtos = navegador.find_elements(By.CLASS_NAME, "product-list")
+
+for produtos in lista_produtos:
+    print(produtos.get_attribute("href").text)
+
+time.sleep(999)
